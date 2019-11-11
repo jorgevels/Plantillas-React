6565ec9054affa0c0010dcf11a613b4fd49cfa6a
jorgevels <jorgevelasquez006@hotmail.com>
2019-11-11 13:42:22 -0500

    Configurando una plantilla para proyectos en React

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "../assets/styles/components/Footer.scss";
+
+const Footer = () => (
+  <footer className="footer">
+    <Link to="/">Terminos de uso</Link>
+    <Link to="/">Declaración de privacidad</Link>
+    <Link to="/">Centro de ayuda</Link>
+  </footer>
+);
+
+export default Footer;
